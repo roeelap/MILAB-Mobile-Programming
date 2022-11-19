60c5a5c1bb99aecfcaaa22a2d7be922a6444e424
roee lapushin <rlapushin@gmail.com>
2022-11-19 18:38:27 +0200

    Added numbers to movie names

--- app/src/main/java/com/example/hw2/SideActivity1.java
+++ app/src/main/java/com/example/hw2/SideActivity1.java
@@ -9,16 +9,16 @@ import android.annotation.SuppressLint;
 public class SideActivity1 extends AppCompatActivity {
 
     private final Movie[] movieList = {
-            new Movie("The Shawshank Redemption", "9.2", R.drawable.shawshank_redemption),
-            new Movie("The Godfather", "9.2", R.drawable.the_godfather),
-            new Movie("The Dark Knight", "9.0", R.drawable.the_dark_knight),
-            new Movie("The Godfather Part II", "9.0", R.drawable.the_godfather_2),
-            new Movie("12 Angry Men", "9.0", R.drawable.angry_men),
-            new Movie("Schindler's List", "8.9", R.drawable.schindler_list),
-            new Movie("The Lord of the Rings: The Return of the King", "8.9", R.drawable.lotr_the_return_of_the_king),
-            new Movie("Pulp Fiction", "8.8", R.drawable.pulp_fiction),
-            new Movie("The Lord of the Rings: The Fellowship of the Ring", "8.8", R.drawable.lotr_the_fellowship_of_the_ring),
-            new Movie("The Good, the Bad and the Ugly", "8.8", R.drawable.the_good_the_bad)
+            new Movie("1. The Shawshank Redemption", "9.2", R.drawable.shawshank_redemption),
+            new Movie("2. The Godfather", "9.2", R.drawable.the_godfather),
+            new Movie("3. The Dark Knight", "9.0", R.drawable.the_dark_knight),
+            new Movie("4. The Godfather Part II", "9.0", R.drawable.the_godfather_2),
+            new Movie("5. 12 Angry Men", "9.0", R.drawable.angry_men),
+            new Movie("6. Schindler's List", "8.9", R.drawable.schindler_list),
+            new Movie("7. The Lord of the Rings: The Return of the King", "8.9", R.drawable.lotr_the_return_of_the_king),
+            new Movie("8. Pulp Fiction", "8.8", R.drawable.pulp_fiction),
+            new Movie("9.The Lord of the Rings: The Fellowship of the Ring", "8.8", R.drawable.lotr_the_fellowship_of_the_ring),
+            new Movie("10. The Good, the Bad and the Ugly", "8.8", R.drawable.the_good_the_bad)
     };
 
     @SuppressLint("DefaultLocale")
--- app/src/main/java/com/example/hw2/SideActivity2.java
+++ app/src/main/java/com/example/hw2/SideActivity2.java
@@ -8,16 +8,16 @@ import android.os.Bundle;
 public class SideActivity2 extends AppCompatActivity {
 
     private final Movie[] movieList = {
-            new Movie("Disaster Movie", "2.1", R.drawable.disaster_movie),
-            new Movie("Manos: The Hands of Fate", "2.2", R.drawable.manos),
-            new Movie("Birdemic: Shock and Terror", "2.3", R.drawable.birdemic),
-            new Movie("Superbabies: Baby Geniuses 2", "2.3", R.drawable.superbabies),
-            new Movie("The Hottie & the Nottie", "2.4", R.drawable.the_hottie),
-            new Movie("House of the Dead", "2.4", R.drawable.house_of_the_dead),
-            new Movie("Son of the Mask", "2.5", R.drawable.son_of_the_mask),
-            new Movie("Saving Christmas", "2.5", R.drawable.saving_christmas),
-            new Movie("Epic Movie", "2.5", R.drawable.epic_movie),
-            new Movie(" Battlefield Earth", "2.6", R.drawable.battlefield_earth)
+            new Movie("1. Disaster Movie", "2.1", R.drawable.disaster_movie),
+            new Movie("2. Manos: The Hands of Fate", "2.2", R.drawable.manos),
+            new Movie("3. Birdemic: Shock and Terror", "2.3", R.drawable.birdemic),
+            new Movie("4. Superbabies: Baby Geniuses 2", "2.3", R.drawable.superbabies),
+            new Movie("5. The Hottie & the Nottie", "2.4", R.drawable.the_hottie),
+            new Movie("6. House of the Dead", "2.4", R.drawable.house_of_the_dead),
+            new Movie("7. Son of the Mask", "2.5", R.drawable.son_of_the_mask),
+            new Movie("8. Saving Christmas", "2.5", R.drawable.saving_christmas),
+            new Movie("9. Epic Movie", "2.5", R.drawable.epic_movie),
+            new Movie("10. Battlefield Earth", "2.6", R.drawable.battlefield_earth)
     };
 
     @Override
